086d92d384124184117b23719f90a795656e6f8f
Luis Fernando <lpetreca@aatb.com.br>
2019-08-21 16:10:00 -0300

    Inital commit - React Calculator

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <React.Fragment>
+        <h1>Calculator</h1>
+        <Calculator />
+    </React.Fragment>
+    , document.getElementById('root')
+);
+
+serviceWorker.unregister();
