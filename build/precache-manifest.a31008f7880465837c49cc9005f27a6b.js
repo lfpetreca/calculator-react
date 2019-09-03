3fdbd5a0ab4a580d0ee14ee3e2ec0a32876c63bb
Luis Fernando <lpetreca@aatb.com.br>
2019-09-03 12:07:00 -0300

    Deploy Heroku

---
+++ build/precache-manifest.a31008f7880465837c49cc9005f27a6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff3218ed60737e181609be0d5defc82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83570c5f35a679e3022f",
+    "url": "/static/css/main.44ed47fd.chunk.css"
+  },
+  {
+    "revision": "e20e1da849a899ad326a",
+    "url": "/static/js/2.48714b8a.chunk.js"
+  },
+  {
+    "revision": "83570c5f35a679e3022f",
+    "url": "/static/js/main.79cdaeb8.chunk.js"
+  },
+  {
+    "revision": "e13205c31f4cbff4253c",
+    "url": "/static/js/runtime~main.b8a35c1f.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
